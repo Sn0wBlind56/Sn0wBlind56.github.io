06333f76c9d4ad5bc91877c8680a36a4b1420aad
Jelle Spoelders <jelle.spoelders@infosupport.com>
2023-09-01 01:08:27 +0200

    Use native CSS smooth scroll instead of jQuery

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -12,29 +12,6 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-// smooth scroll
-$(document).ready(function(){
-    $(".nav .link").on('click', function(event) {
-        // Should have been set automatically through href by clicking the link
-        if (this.hash === "") {
-            return;
-        }
-
-        var indexPage = window.location.pathname.indexOf("index.html");
-
-        if (indexPage && indexPage > 0) {
-            var hash = this.hash;
-            event.preventDefault();
-    
-            $('html').animate({
-                scrollTop: $(hash).offset().top
-            }, 500, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-});
-
 // navbar toggle
 $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
